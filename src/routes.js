882d6ce4e9fb96babd9543dda939e3f247b814e7
Victor Sui <victorsui11@gmail.com>
2017-08-01 10:30:32 -0500

    Add basic auth flow

---
+++ src/routes.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Route, BrowserRouter } from 'react-router-dom';
+
+import Auth from './auth/Auth';
+import history from './history';
+
+import App from './pages/App';
+import Callback from './pages/Callback'
+
+const auth = new Auth();
+
+const handleAuthentication = (nextState, replace) => {
+  if (/access_token|id_token|error/.test(nextState.location.hash)) {
+    auth.handleAuthentication();
+  }
+};
+
+export const makeMainRoutes = () => {
+  return (
+    <BrowserRouter history={ history } component={ App }>
+      <div>
+        <Route path="/" render={ (props) =>
+          <App auth={ auth } {...props}/>
+        }/>
+        <Route path="/callback" render={ (props) => {
+          handleAuthentication(props);
+          return <Callback {...props} />;
+        }}/>
+      </div>
+    </BrowserRouter>
+  );
+}
