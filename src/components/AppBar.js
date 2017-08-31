d0c2f1a1d9273d736411ee1ef1a66140c2616aef
Victor Sui <victorsui11@gmail.com>
2017-08-30 20:51:20 -0500

    Remove styling from AppBar

--- src/components/AppBar.js
+++ src/components/AppBar.js
@@ -1,22 +1,7 @@
 import React, { Component } from 'react';
 
 import Avatar from 'material-ui/Avatar';
-import MaterialAppBar from 'material-ui/AppBar';
 import Button from 'material-ui/Button';
-import Toolbar from 'material-ui/Toolbar';
-import Typography from 'material-ui/Typography';
-
-import { withStyles, createStyleSheet } from 'material-ui/styles';
-
-const styleSheet = createStyleSheet({
-  root: {
-    marginTop: 30,
-    width: '100%'
-  },
-  flex: {
-    flex: 1
-  }
-});
 
 class AppBar extends Component {
   state = { profile: {} };
@@ -41,28 +26,15 @@ class AppBar extends Component {
     const { isAuthenticated } = props.auth;
 
     return (
-      <MaterialAppBar position="static">
-        <Toolbar>
-          <Typography
-            type="title"
-            color="inherit"
-            className={ props.classes.flex }
-          >
-            Cucumber
-          </Typography>
-          {
-            isAuthenticated()
-            ? <Button color="contrast" onClick={ this.logout }>Logout</Button>
-            : <Button color="contrast" onClick={ this.login }>Login</Button>
-          }
-          {
-            profile
-            && <Avatar src={ profile.picture }/>
-          }
-        </Toolbar>
-      </MaterialAppBar>
+      <div>
+        <h1>Cucumber</h1>
+        { isAuthenticated()
+          ? <Button onClick={ this.logout }>Logout</Button>
+          : <Button onClick={ this.login }>Login</Button> }
+        { profile && <Avatar src={ profile.picture }/> }
+      </div>
     );
   }
 };
 
-export default withStyles(styleSheet)(AppBar);
+export default AppBar;
